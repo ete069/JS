3c7043daebb347484df82e8b48d28ee306e1c440
Azaki <zakidawud69@gmail.com>
2025-10-31 08:06:34 +0700

    first commit

---
+++ hello.js
@@ -0,0 +1,16 @@
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <title>Hello World</title>
+// </head>
+// <body>
+// <script>
+//     document.write("Hello, World!")
+// </script>
+// </body>
+// </html>
+
+// Membuat hello dengan JavaScript dalam HTML sederhana
+
+document.writeln("Hello, World!");
