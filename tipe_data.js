52b233d1b804b7105e3ea7e96864bc8ac465a1d4
Azaki <zakidawud69@gmail.com>
2025-11-16 09:27:19 +0700

    Dasar

---
+++ tipe_data.js
@@ -0,0 +1,18 @@
+console.info(10); // number
+console.info(20);
+console.info(0b10101010); // biner
+console.info(0x6699); // hexadecimal
+console.info(0o66,99); //octadecimal
+console.info(true); // boolean
+console.info(false);
+console.info("Abdurahman Zaki"); // string
+console.info(" ");
+console.info('D'); // char
+console.info("Nama : " + "Zaki");
+console.info("Abdurahman \nZaki \nD"); // newline
+console.info("Nama \t: Zaki"); // tab
+console.info("\\C\\user\\zaki"); // slash
+console.info("\"Selamat Datang\""); // daouble quote
+console.info("\'Sampai Jumpa\'"); // single quote
+
+//Program ini memperlihatkan berbagai tipe data dalam javascript
