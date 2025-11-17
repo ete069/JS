f2093c854db55bdfb271f952e69dd557487b22a0
Azaki <zakidawud69@gmail.com>
2025-11-17 18:08:53 +0700

    Dasar

---
+++ null.js
@@ -0,0 +1,11 @@
+let nama = null;
+
+if (nama === undefined){
+    console.info("Undifined");
+} else if (nama === null){
+    console.info("Null");
+} else {
+    console.info(nama);
+}
+
+// Program ini menampilkan data dengan nilai null atau nilai kosong
