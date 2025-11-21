8036a069423de27b489ddcb1cf9890cd1299c986
Azaki <zakidawud69@gmail.com>
2025-11-21 19:37:51 +0700

    Dasar

---
+++ undefined.js
@@ -0,0 +1,22 @@
+let nama;
+if (nama === undefined){
+    console.info("Tidak ada datanya!");
+} else {
+    console.info("Ada datanya");
+}
+
+let hewan = ["Kucing", "Anjing"]; // dalam array
+if (hewan[2] === undefined){
+    console.info("Tidak ada dalam data!");
+} else {
+    console.info("Data ada.");
+}
+
+let orang ={}; // dalam object
+if (orang.nama === undefined){
+    console.info("Objek tidak ada!");
+} else {
+    console.info("Objek ada.");
+}
+
+// Program ini memperlihatkan tipe data undifined, yaitu data yang belum diberi nilai di dalamnya
